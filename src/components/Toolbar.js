26344de18b486ca5c3305ab8913a07c67aaa8def
Greg Douglas <gwshark0@gmail.com>
2019-05-07 12:14:31 -0400

    Add title

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { FormattedMessage, defineMessages } from 'react-intl';
 import Search from './Search';
 
 import './Toolbar.scss';
 
+const messages = defineMessages({
+  title: 'Hello world',
+});
+
 function Toolbar() {
   return (
     <nav className="toolbar">
+      <h1 className="toolbar__title">
+        <FormattedMessage {...messages.title} />
+      </h1>
       <Search />
     </nav>
   );
